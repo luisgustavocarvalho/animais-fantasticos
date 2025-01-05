99fcf4bef16ebb852d4763a5910581dd73ac166d
Bowie <lgustavo.carvalho07@gmail.com>
2025-01-05 13:26:10 -0300

    criado um script para entender os conceitos de algumas selecoes e comentado para nao interferir no codigo

---
+++ script-aula.js
@@ -0,0 +1,23 @@
+// const animais = document.getElementById("animais");
+// console.log(animais);
+
+// const gridSection = document.getElementsByClassName("grid-section");
+// console.log(gridSection[1]);
+
+// const primeiraLi = document.querySelector("li");
+// console.log(primeiraLi);
+
+// const primeiraUl = document.querySelector("ul");
+// console.log(primeiraUl);
+
+// const linkInternos = document.querySelector('[href^="#"');
+// console.log(linkInternos);
+
+// const animaisImg = document.querySelectorAll(".animais img");
+// console.log(animaisImg[1]);
+
+// const gridSectionHTML = document.getElementsByClassName("grid-section");
+// const gridSectionNode = document.querySelectorAll(".grid-section");
+
+// console.log(gridSectionHTML);
+// console.log(gridSectionNode);
